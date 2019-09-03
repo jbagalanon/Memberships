db8af35846fe685187fa17ccda38c6a5a5e86caa
Joemar <Joemar@GINO>
2019-09-03 23:11:49 +0800

    Database set to local

--- Memmberships/Migrations/Configuration.cs
+++ Memmberships/Migrations/Configuration.cs
@@ -9,7 +9,8 @@ namespace Memmberships.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Memmberships.Models.ApplicationDbContext context)
